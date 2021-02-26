e06be3ad8c14aec996308a8b6bc9d8ab995496ef
1018qqbook <79676833+1018qqbook@users.noreply.github.com>
2021-02-26 15:22:10 +0800

    Add files via upload

---
+++ d.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    if(n>=90) printf("A");
+    if(n<90 && n>=80) printf("B");
+    if(n<80 && n>=60) printf("C");
+    else printf("F");
+}
